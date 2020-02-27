f40321b99dd2926aa0a0dacd9e834919b3df8446
Andrei Andreev <andreev.aa@phystech.edu>
2020-02-27 14:44:11 +0300

    Hello, World!

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+int main() // Программы на C++ начинаются с выполнения функции
+// main
+{
+cout << "Hello, World!\n"; // вывод строки "Hello, World!"
+keep_window_open(); // ожидание ввода символа
+return 0;
+}
